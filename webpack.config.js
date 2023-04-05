cc7676d8c937a3f5728f9a68eac04233dd24ffe9
Ismail Al Ghani <ismailalghani@gmail.com>
2023-04-05 18:28:29 +0700

    add new configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
-const path = require("path");
+const { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "build"),
+  mode: prod ? "production" : "development",
+  devtool: prod ? false : "eval-source-map",
+  entry: {
+    app: resolve(__dirname, "src/index"),
+  },
+  resolve: {
+    modules: ["node_modules"],
+    mainFiles: ["index"],
+    extensions: [".ts", ".tsx", ".js", ".mjs", ".jsx", ".json"],
   },
   module: {
     rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -19,24 +33,46 @@ module.exports = {
         loader: "ts-loader",
       },
       {
-        use: ["style-loader", "css-loader"],
         test: /\.css$/,
+        use: [
+          prod ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+        ],
       },
     ],
   },
-  resolve: {
-    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
+      hash: true,
+      template: "public/index.html",
+      minify: {
+        collapseWhitespace: true,
+      },
     }),
+    new CleanWebpackPlugin(),
+    ...(prod
+      ? [
+          new MiniCssExtractPlugin({
+            filename: "static/css/[name].[fullhash:8].css",
+            chunkFilename: "static/css/[name].[fullhash:8].chunk.css",
+          }),
+        ]
+      : []),
   ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "build"),
-    },
-    port: 3000,
-    historyApiFallback: true,
+  optimization: {
+    minimize: prod,
+    minimizer: [
+      new TerserWebpackPlugin({
+        parallel: true,
+        terserOptions: {
+          safari10: true,
+        },
+      }),
+    ],
+  },
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "static/js/[name].[fullhash:8].js",
+    chunkFilename: "static/js/[name].[fullhash:8].chunk.js",
   },
 };
